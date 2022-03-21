6c5122fcabee983006eb81e05ed936851e2d85bb
whitel <heriskent@gmail.com>
2022-03-21 11:51:42 +0800

    Update

--- code_template/main.cpp
+++ code_template/main.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+#define DEBUG
+
 class Solution {
 public:
     void run() {
@@ -10,11 +12,20 @@ public:
 };
 
 int main() {
+#ifdef DEBUG
     freopen("inputdata", "r", stdin);
+    cout << "===========INPUT=============" << endl;
+    system("cat inputdata");
+    cout << endl;
+    cout << "===========OUTPUT============" << endl;
+ #endif
+
     Solution s;
-
-
-    
     s.run();
+
+#ifdef DEBUG
+    cout << "============END==============" << endl;
+#endif
     return 0;
 }
+
